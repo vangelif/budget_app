c07dedb0dc7cf669015633dff01ddec25e48184a
vangelif <ev.fotiades@yahoo.com>
2024-01-12 15:34:04 +0200

    build tests

--- spec/requests/splash_spec.rb
+++ spec/requests/splash_spec.rb
@@ -1,10 +1,19 @@
+# spec/requests/splash_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe 'Splashes', type: :request do
+  before(:each) do
+    # Set the default_url_options for ActionMailer
+    Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/splash/index'
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
+
+  # You can add more tests for other actions in the splash controller if needed
 end
